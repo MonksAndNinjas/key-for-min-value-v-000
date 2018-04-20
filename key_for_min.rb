ec0629c8c8f06a03eb41530b8f04bdd5f49d81d3
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-20 05:58:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,17 @@
 def key_for_min_value(name_hash)
   name_hash.each do |name, number|     #name_hash[name] = number
     name_hash[name] = number
-      name_hash.each do |name_1, number_1|
-        if (name_hash[name_1] = number_1 <=> name_hash[name] = number) == 1
-          break
-        end
+
+
+
+
+      name_hash.collect do |name_1, number_1|
+        name_hash[name_1] = number_1 <=> name_hash[name] = number
+        number_1 = number
       end
+
+
+
+
   end
 end
