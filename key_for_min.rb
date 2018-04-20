84a70f13b110dcee5c1e77644d68198111d771a6
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-20 07:03:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,7 +11,7 @@ names = []
 
     if
       (name_hash.each do |name_1, number_1|
-          if (number_1 <=> number) === 1 or name_1 == name
+          if (number_1 <=> number) == 1 or name_1 === name
             names<<name_1
           end
       end).size ==  #returns an array
