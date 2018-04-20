27c0ec1d340947b66f7342a41e87faa678bbb3c6
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-20 06:38:23 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,10 +11,8 @@ def key_for_min_value(name_hash)
 
     if
       (name_hash.collect do |name_1, number_1|
-        if (number_1 <=> number) == 1 or number_1 == number
-          number_1
-        end
-      end).size == 1 #returns an array
+        number_1 <=> number) == 1 or number_1 == number
+      end).size ==  #returns an array
               return  name
      end # outputs a collection of arrays such that |name, number| => [array with numbers greater than or equal to number]
   end
