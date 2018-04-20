4ec0c5cf704f15ede2964211417ae8a1c524b42f
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-20 06:27:03 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,17 +6,17 @@ def key_for_min_value(name_hash)
 
 
 
-  (name_hash.each do |name, number|     #this is a hash
+  name_hash.each do |name, number|     #this is a hash
   #  name_hash[name] = number
 
-    if 
+    if
       (name_hash.collect do |name_1, number_1|
-        if (number_1 <=> number) == 1 or number_1 == number 
-          number_1 
+        if (number_1 <=> number) == 1 or number_1 == number
+          number_1
         end
       end).size == 1 #returns an array
                    name
-  end) # outputs a collection of arrays such that |name, number| => [array with numbers greater than or equal to number]
-    
+  end # outputs a collection of arrays such that |name, number| => [array with numbers greater than or equal to number]
+
   #now i need to compare (keypair => [ >= number]) length and see which is greater
 end
